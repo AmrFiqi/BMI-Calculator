84188a579a35d0b88fe7fd3e84cf963508daadc9
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-04-07 12:34:31 +0200

    created IBActions that changes the height and weight label base on the slider bar

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var weightLabel: UILabel!
+    
+    // MARK: - Class Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + " m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.2f", sender.value) + " Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
